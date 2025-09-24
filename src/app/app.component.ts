c61c34e84cd414d582669b898f584a12c9ab8650
Nicolas Caicedo <ng.caicedo@uniandes.edu.co>
2025-09-24 13:30:58 -0500

    Add CustomTextFieldComponent with template, styles, and integration into AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { CustomButtonComponent } from './components/custom-button/custom-button.component';
+import { CustomTextFieldComponent } from './components/custom-text-field/custom-text-field.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CustomButtonComponent],
+  imports: [RouterOutlet, FormsModule, CustomButtonComponent, CustomTextFieldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'alerti';
 
+  usuario = '';
+
   onButtonClick(buttonType: string): void {
     console.log(`Button clicked: ${buttonType}`);
+  }
+
+  onTextFieldChange(field: string, value: string): void {
+    console.log(`${field} changed to: ${value}`);
   }
 }
---
+++ src/app/components/custom-text-field/custom-text-field.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-custom-text-field',
+  imports: [FormsModule, MatInputModule, MatFormFieldModule],
+  templateUrl: './custom-text-field.component.html',
+  styleUrl: './custom-text-field.component.scss'
+})
+export class CustomTextFieldComponent {
+  @Input() label: string = '';
+  @Input() value: string = '';
+  @Input() placeholder: string = '';
+  @Output() valueChange = new EventEmitter<string>();
+  @Output() inputChange = new EventEmitter<string>();
+
+  onValueChange(newValue: string): void {
+    this.value = newValue;
+    this.valueChange.emit(newValue);
+    this.inputChange.emit(newValue);
+  }
+}
